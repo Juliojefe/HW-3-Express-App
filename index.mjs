257b58d25b0c6943bc9b66a9d75296edf91e8c94
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2025-10-29 15:10:14 -0700

    second rout done

--- index.mjs
+++ index.mjs
@@ -3,6 +3,21 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// helper function to fetch multiple character objects from an array of URLs
+async function fetchResidents(residentUrls) {
+  const residents = [];
+  if (!residentUrls || residentUrls.length === 0) {
+    return residents;
+  }
+  for (let i = 0; i < residentUrls.length; ++i) {
+   const url = residentUrls[i];
+   const response = await fetch(url);
+   const data = await response.json();
+   residents.push(data);
+  }
+  return residents;
+}
+
 app.get("/characters", async (req, res) => {
    let page;
    if (req.query.page) {   // default to page 1 if no page provided
@@ -10,9 +25,9 @@ app.get("/characters", async (req, res) => {
    } else {
       page = 1;
    }
-   const url = `https://rickandmortyapi.com/api/character/?page=${page}`;
-   const rawData = await fetch(url);
-   const cookedData = await rawData.json();
+   let url = `https://rickandmortyapi.com/api/character/?page=${page}`;
+   let rawData = await fetch(url);
+   let cookedData = await rawData.json();
    let nextPage = null;
    let prevPage = null;
    if (cookedData.info.next !== null) {
@@ -32,6 +47,25 @@ app.get("/characters", async (req, res) => {
 
 app.get("/", (req, res) => {
    res.redirect("/characters");
+});
+
+app.get("/character/:id", async (req, res) => {
+   let charId = req.params.id;
+   let charRawData = await fetch("https://rickandmortyapi.com/api/character/" + charId);
+   let charCookedData = await charRawData.json();
+   let originRawData = null;
+   let originCookedData = null;
+   let residentsData = null;
+   if (charCookedData.origin.url) {
+      originRawData = await fetch(charCookedData.origin.url);
+      originCookedData = await originRawData.json();
+      residentsData = await fetchResidents(originCookedData.residents);
+   }
+   res.render("character.ejs", {
+      character: charCookedData,
+      residents: residentsData,
+      active: "none"
+   });
 });
 
 app.listen(3000, () => {
