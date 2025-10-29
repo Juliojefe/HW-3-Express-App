7c8f5d4069fc28984dca48cf10c960a92d91972d
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2025-10-28 22:43:17 -0700

    card chars

--- index.mjs
+++ index.mjs
@@ -3,10 +3,30 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-   // res.send('Hello Express app!')
-   res.render("character.ejs");
+let currPage = "https://rickandmortyapi.com/api/character/?page=1";
+
+app.get('/', async (req, res) => {
+   if (currPage == null || currPage.length <= 0) {
+      currPage = "https://rickandmortyapi.com/api/character/?page=1";
+   }
+   const rawRes = await fetch(currPage);
+   const cookedRes = await rawRes.json();
+   currPage = cookedRes.info.next;
+   res.render("characters.ejs", { characters: cookedRes.results, hasNext: cookedRes.info.next !== null });
+
 });
+
+// app.get('/', (req, res) => {
+//    res.render("character.ejs");
+// });
+
+// app.get('/', (req, res) => {
+//    res.render("character.ejs");
+// });
+
+// app.get('/', (req, res) => {
+//    res.render("character.ejs");
+// });
 
 app.listen(3000, () => {
    console.log('server started');
